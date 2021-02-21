35cb9973dd20f176238d67bfe3951f8cdd168eba
nicktorbich <nicktorbich@protonmail.com>
2021-02-21 19:30:22 +0100

    Add vscode settings.json, keybindings.json and extensions

--- pandora.sh
+++ pandora.sh
@@ -10,4 +10,12 @@ ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm -rf $HOME/.vimrc
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 
+# Removes vscode settings.json and keybindings.json from $HOME (if it exists) and symlinks the settings.json and keybindings.json files from the dotfiles
+rm -rf $HOME/Library/Application\ Support/Code/User/settings.json
+rm -rf $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+source vscode/extensions.sh
+
 echo "Set up complete"
